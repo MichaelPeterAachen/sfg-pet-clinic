91f275c6924b376f382f326496311ba912f8be39
Michael Peter <Michael.Peter@saurer.com>
2023-04-05 14:57:45 +0200

    Enhance data.sql

--- pet-clinic-web/src/main/resources/data.sql
+++ pet-clinic-web/src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO types (name) VALUES ('dog')
 INSERT INTO types (name) VALUES ('cat')
+INSERT INTO types (name) VALUES ('pinguin')
 INSERT INTO owners (first_name, last_name) VALUES ('Michael', 'Pezer')
